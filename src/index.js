7e068b1bef76b8967e8dcb343067aaf38c11ec19
Rafael Dias <rafael.dias@gmail.com>
2018-03-17 19:01:37 -0300

    add logger middleware

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {Provider} from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-
-import App from './components/App';
-// import 'spectre.css/dist/spectre.min.css'
-// import './style.css'
+import 'spectre.css/dist/spectre.min.css'
+import './style.css'
 import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
 
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const store = createStoreWithMiddleware(reducers);
+// cria middlewares
+const logger = store => next => action => {
+    console.group(action.type);
+    console.info('dispatching', action);
+    let result = next(action);
+    console.log('next state', store.getState());
+    console.groupEnd(action.type);
+    return result;
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducers,
+    composeEnhancers(
+        applyMiddleware(logger),
+        applyMiddleware(logger)
+    )
+);
 
 render(
     <Provider store={store}>
